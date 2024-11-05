69dc31d12d4a6fe484d645b345622fec8e8cdbcd
shareefevans <shareef3vans@gmail.com>
2024-11-05 22:16:38 +1100

    renaming the file structure

--- Develop/Main/src/components/Header/index.jsx
+++ Develop/Main/src/components/Header/index.jsx
@@ -1,10 +1,9 @@
 import coverImage from '../../assets/cover/cover-image.jpg';
 
 function Header(props) {
-
   return (
     <header className="flex-row space-between px-1">
-      <h1>Lernantino</h1>
+      <h1>Shareef Evans</h1>
       <img src={coverImage} alt="wooden background"></img>
       {props.children}
     </header>
